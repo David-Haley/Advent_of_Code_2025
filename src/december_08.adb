888bd108535ef39db183507e73fc46ddd0ff3114
David-Haley <34273510+David-Haley@users.noreply.github.com>
2025-12-29 22:01:43 +1000

    Day 08 part one, finally interpreted the pairs rether than connections corectly.

--- src/december_08.adb
+++ src/december_08.adb
@@ -12,7 +12,7 @@ with DJH.Execution_Time; use DJH.Execution_Time;
 
 procedure December_08 is
 
-   subtype Ordinates is Positive;
+   subtype Ordinates is Long_Long_Integer;
 
    type Coordinates is record
       X, Y, Z : Ordinates;
@@ -59,7 +59,8 @@ procedure December_08 is
 
    package Pair_Sorting is new Pair_Stores.Generic_Sorting;
 
-   procedure Read_Input (Box_Store : out Box_Stores.Vector) is
+   procedure Read_Input (Box_Store : out Box_Stores.Vector;
+                         Max_Pairs : out Positive) is
 
       Input_File : File_Type;
       Text : Unbounded_String;
@@ -73,6 +74,11 @@ procedure December_08 is
       else
          Open (Input_File, In_File, Argument (1));
       end if; -- Argument_Count = 0
+      if Argument_Count = 2 then
+         Max_Pairs := Positive'Value (Argument (2));
+      else
+         Max_Pairs := 1000;
+      end if; -- Argument_Count = 2
       Clear (Box_Store);
       while not End_Of_File (Input_File) loop
          Get_Line (Input_File, Text);
@@ -94,9 +100,7 @@ procedure December_08 is
    end Read_Input;
 
    function Distance (P1, P2 : Coordinates) return Distances is
-     (Distances ((P1.X - P2.X) ** 2) +
-        Distances ((P1.Y - P2.Y) ** 2) +
-          Distances ((P1.Z - P2.Z) ** 2));
+     ((P1.X - P2.X) ** 2 + (P1.Y - P2.Y) ** 2 + (P1.Z - P2.Z) ** 2);
    --  More correctly this returns the square of the distsnce!
 
    procedure Build_Pair_Store (Box_Store : Box_Stores.Vector;
@@ -118,38 +122,30 @@ procedure December_08 is
 
    procedure Build_Circuits (Box_Store : in out Box_Stores.Vector;
                              Pair_Store : Pair_Stores.List;
-                             Circuit_Store : out Circuit_Stores.Vector) is
+                             Circuit_Store : out Circuit_Stores.Vector;
+                             Max_Pairs : Positive) is
 
       P : Pair_Stores.Cursor := First (Pair_Store);
-      Cable_Count : Natural := 0;
-      Max_Circuits : constant Natural := 10;
+      Pair_Count : Natural := 0;
       Common : Box_Sets.Set;
 
    begin -- Build_Circuits
       Clear (Circuit_Store);
-      --  while P /= Pair_Stores.No_Element and then
-      --    Cable_Count < Max_Circuits loop
-      while P /= Pair_Stores.No_Element loop
-         if Intersection (Box_Store (Element (P).B1).Connected,
-           Box_Store (Element (P).B2).Connected) = Box_Sets.Empty_Set
+      while P /= Pair_Stores.No_Element and then
+        Pair_Count < Max_Pairs loop
+         Pair_Count := @ + 1;
+         if Is_Empty (Intersection (Box_Store (Element (P).B1).Connected,
+           Box_Store (Element (P).B2).Connected))
          then
-            --  Can be connected, not in same circuit
-            Clear (Common);
+            --  Can be connected, no common junction box
+            Common := Union (Box_Store (Element (P).B1).Connected,
+                             Box_Store (Element (P).B2).Connected);
             Include (Common, Element (P).B1);
             Include (Common, Element (P).B2);
-            for CB1 in Iterate (Box_Store (Element (P).B1).Connected) loop
-               Union (Common, Box_Store (Element (CB1)).Connected);
-            end loop; -- CB1 in Iterate (Box_Store (Element (P).B1).Connected)
-            for CB2 in Iterate (Box_Store (Element (P).B2).Connected) loop
-               Union (Common, Box_Store (Element (CB2)).Connected);
-            end loop; -- CB2 in Iterate (Box_Store (Element (P).B2).Connected)
-            Cable_Count := @ + 1;
-            --  Put_Line ("Conection (" & Cable_Count'Img & ')' & Element (P).B1'Img & Element (P).B2'Img);
             for C in Iterate (Common) loop
                Box_Store (Element (C)).Connected := Copy (Common);
-               --  Put_Line (Element (C)'Img & Box_Store (Element (C)).Connected'Img);
             end loop; -- C in Iterate (Common)
-         end if; -- Intersection (Box_Store (Element (P).B1).Connected ...
+         end if; --  Is_Empty (Intersection (Box_Store (Element (P).B1) ...
          Next (P);
       end loop; -- P /= Pair_Stores.No_Element and then ...
       for B in Iterate (Box_Store) loop
@@ -165,13 +161,12 @@ procedure December_08 is
    Box_Store : Box_Stores.Vector := Box_Stores.Empty_Vector;
    Pair_Store : Pair_Stores.List := Pair_Stores.Empty_List;
    Circuit_Store : Circuit_Stores.Vector := Circuit_Stores.Empty_Vector;
+   Max_Pairs : Positive;
 
 begin -- December_08
-   Read_Input (Box_Store);
+   Read_Input (Box_Store, Max_Pairs);
    Build_Pair_Store (Box_Store, Pair_Store);
-   --  Put_Line (Pair_Store'Img);
-   Build_Circuits (Box_Store, Pair_Store, Circuit_Store);
-   Put_Line (Circuit_Store'Img);
+   Build_Circuits (Box_Store, Pair_Store, Circuit_Store, Max_Pairs);
    Put_Line ("Part one:" & Count_Type'Image (Length (Circuit_Store (1)) *
                Length (Circuit_Store (2)) * Length (Circuit_Store (3))));
    Put_CPU_Time;
