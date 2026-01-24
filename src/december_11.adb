aa25d6b6a844de45c5132c490ac5457243c3cfea
David-Haley <34273510+David-Haley@users.noreply.github.com>
2026-01-24 21:03:50 +1000

    Day 11 part 2 and AOC2025 completed. Cacheing results works a treat!

--- src/december_11.adb
+++ src/december_11.adb
@@ -5,7 +5,6 @@ with Ada.Strings; use Ada.Strings;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with Ada.Strings.Maps; use Ada.Strings.Maps;
 with Ada.Containers.Ordered_Maps;
-with Ada.Containers.Ordered_Sets;
 with Ada.Containers.Doubly_Linked_Lists;
 with DJH.Execution_Time; use DJH.Execution_Time;
 
@@ -19,9 +18,6 @@ procedure December_11 is
    package Device_Maps is new
      Ada.Containers.Ordered_Maps (Devices, Device_Lists.List);
    use Device_Maps;
-
-   package Visited_Lists is new Ada.Containers.Ordered_Sets (Devices);
-   use Visited_Lists;
 
    subtype Big_Natural is Long_Long_Integer range 0 .. Long_Long_Integer'Last;
 
@@ -63,89 +59,102 @@ procedure December_11 is
       Close (Input_File);
    end Read_Input;
 
-   function Count_Paths (Device_Map : Device_Maps.Map) return Natural is
-
-      package Visited_Lists is new Ada.Containers.Ordered_Sets (Devices);
-      use Visited_Lists;
+   function Count_Paths (Device_Map : Device_Maps.Map)
+                         return Big_Natural is
 
       Search_Start : constant Devices := "you";
       Search_End : constant Devices := "out";
 
       procedure Search (Device_Map : Device_Maps.Map;
                         Current : Devices;
-                        Visited_List : in out Visited_Lists.Set;
-                        Result : in out Natural) is
+                        Result : in out Big_Natural) is
 
       begin -- Search
          if Current = Search_End then
             Result := @ + 1;
          elsif Contains (Device_Map, Current) then
             for N in Iterate (Device_Map (Current)) loop
-               if not Contains (Visited_List, Element (N)) then
-                  Include (Visited_List, Element (N));
-                  Search (Device_Map, Element (N), Visited_List, Result);
-                  Exclude (Visited_List, Element (N));
-               end if; -- not Contains (Visited_List, Element (N))
+               Search (Device_Map, Element (N), Result);
             end loop; -- N in Iterate (Device_Map (Current))
          end if; -- Current = Search_End
       end Search;
 
-      Result : Natural := 0;
-      Visited_List : Visited_Lists.Set := Visited_Lists.Empty_Set;
+      Result : Big_Natural := 0;
 
    begin -- Count_Paths
-      Search (Device_Map, Search_Start, Visited_List, Result);
+      Search (Device_Map, Search_Start, Result);
       return Result;
    end Count_Paths;
 
    function Count_Paths_2 (Device_Map : Device_Maps.Map) return Big_Natural is
 
+      subtype Cache_Keys is String (1 .. 4);
+
       package Caches is new
-        Ada.Containers.Ordered_Maps (Devices, Big_Natural);
+        Ada.Containers.Ordered_Maps (Cache_Keys, Big_Natural);
       use Caches;
 
+      function Make_Key (Device : Devices;
+                         FFT_Found, DAC_Found : Boolean) return Cache_Keys is
+
+         Result : Cache_Keys;
+
+      begin -- Make_Key
+         Result (1 .. 3) := Device;
+         if FFT_Found then
+            if DAC_Found then
+               Result (4) := '3';
+            else
+               Result (4) := '2';
+            end if; -- DAC_Found
+         else
+            if DAC_Found then
+               Result (4) := '1';
+            else
+               Result (4) := '0';
+            end if; -- DAC_Found
+         end if; -- FFT_Found
+         return Result;
+      end Make_Key;
+
       Search_Start : constant Devices := "svr";
+      FFT : constant Devices := "fft";
+      DAC : constant Devices := "dac";
       Search_End : constant Devices := "out";
 
       function Search (Device_Map : Device_Maps.Map;
                        Current : Devices;
-                       Must_Visit : Visited_Lists.Set;
-                       Visited_List : in out Visited_Lists.Set;
+                       FFT_Found, DAC_Found : Boolean;
                        Cache : in out Caches.Map)
                        return Big_Natural is
 
+         Key : constant Cache_Keys := Make_Key (Current, FFT_Found, DAC_Found);
+         FFT_Found_Out, DAC_Found_Out : Boolean;
          Result : Big_Natural := 0;
 
       begin -- Search
-         if Contains (Cache, Current) then
-            Result := Cache (Current);
-         elsif Current = Search_End and then
-           Is_Subset (Must_Visit, Visited_List)
-         then
-            Result := 1;
+         if Contains (Cache, Key) then
+            Result := Cache (Key);
+         elsif Current = Search_End then
+            if FFT_Found and then DAC_Found then
+               Result := 1;
+            end if; -- FFT_Found and then DAC_Found
          elsif Contains (Device_Map, Current) then
+            FFT_Found_Out := FFT_Found or else Current = FFT;
+            DAC_Found_Out := DAC_Found or else Current = DAC;
             for N in Iterate (Device_Map (Current)) loop
-               if not Contains (Visited_List, Element (N)) then
-                  Include (Visited_List, Element (N));
-                  Result := @ + Search (Device_Map, Element (N), Must_Visit,
-                                        Visited_List, Cache);
-                  Exclude (Visited_List, Element (N));
-               end if; -- not Contains (Visited_List, Element (N))
+               Result := @ + Search (Device_Map, Element (N), FFT_Found_Out,
+                                     DAC_Found_Out, Cache);
             end loop; -- N in Iterate (Device_Map (Current))
          end if; -- Contains (Cache, Key)
-         Include (Cache, Current, Result);
+         Include (Cache, Key, Result);
          return Result;
       end Search;
 
-      Visited_List : Visited_Lists.Set := Visited_Lists.Empty_Set;
-      Must_Visit : Visited_Lists.Set := Visited_Lists.Empty_Set;
       Cache : Caches.Map := Caches.Empty_Map;
 
    begin -- Count_Paths_2
-      Include (Must_Visit, "dac");
-      Include (Must_Visit, "fft");
-      return Search (Device_Map, Search_Start, Must_Visit, Visited_List,
-                     Cache);
+      return Search (Device_Map, Search_Start, False, False, Cache);
    end Count_Paths_2;
 
    Device_Map : Device_Maps.Map := Device_Maps.Empty_Map;
